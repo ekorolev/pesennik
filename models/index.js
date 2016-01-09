5aada6c315975bc6a8f6044c89f15df2d30f9362
ekorolev <ea.korolev@mail.ru>
2016-01-09 21:34:03 +0300

    prepare user schema in module

--- models/index.js
+++ models/index.js
@@ -1,12 +1,10 @@
 module.exports = function (mongoose) {
 	
-	var users = require('./users');
 	var authors = require('./authors');
 	var sings = require('./sings');
 	var comments = require('./comments');
 
 	return {
-		users: users(mongoose),
 		authors: authors(mongoose),
 		sings: sings(mongoose),
 		comments: comments(mongoose)
--- models/users.js
+++ models/users.js
@@ -3,36 +3,16 @@ module.exports = function (mongoose) {
 	var Schema = mongoose.Schema({
 		login: String,
 		collapseIn: Boolean,
-		password: String,
 		firstname: String,
 		secondname: String,
 		vklink: String,
 		bdate: Date,
 		regTime: Date,
 		admin: Boolean,
-		cryptNow: Boolean,
 		singCount: { type: Number, default: 0 },
 		lastActivity: Date,
 		role: String
 	});
-	Schema.pre('save', function (next) {
-		var _self = this;
-		if (_self.isNew || _self.cryptNow) {
-			bcrypt.hash(_self.password, 8, function (err, hash) {
-				if (err) next(new Error('Error')); else {
-					_self.password = hash;
-					_self.cryptNow = false;
-					next();
-				}
-			});
-		} else {
-			next();
-		}
-	});
-	Schema.methods.compare = function (cnd, cb) {
-		var _self = this;
-		bcrypt.compare(cnd, _self.password, cb);
-	}
 
 	Schema.methods.secureInfo = function () {
 		var self = this;
@@ -52,6 +32,6 @@ module.exports = function (mongoose) {
 			lastActivity: self.lastActivity? activity.format('yyyy-mm-dd HH:MM:ss') : 'none',
 		};
 	}
-	var model = mongoose.model('users', Schema);
-	return model;
+
+	return Schema;
 }
